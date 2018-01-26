3b805c25dee06342cb9669c9664d60093e6b56d5
hollowspecter <hollowspecder@googlemail.com>
2018-01-27 00:06:57 +0100

    added occasional ice break sounds

---
+++ Assets/Audio/Ambiance.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Ambiance : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip[] clips;
+    [SerializeField]
+    private Vector2 minMaxDuration = new Vector2(1f, 4f);
+
+    private AudioSource _audio;
+
+    void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(PlayRando());
+    }
+
+    IEnumerator PlayRando()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minMaxDuration.x, minMaxDuration.y));
+            _audio.PlayOneShot(clips[Random.Range(0, clips.Length)], 0.5f);
+        }
+    }
+}
