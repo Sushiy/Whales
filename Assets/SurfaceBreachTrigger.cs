1d061f06c6da4c58b254943a28bc9ceb39ab5b1c
Sushiy <julivizi@yahoo.de>
2018-01-28 03:27:14 +0100

    Sounds added to my Scene

---
+++ Assets/SurfaceBreachTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfaceBreachTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 8)
+            ClipPlayer.s_clipPlayer.PlaySurface();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 8)
+            ClipPlayer.s_clipPlayer.PlaySurface();
+    }
+}
