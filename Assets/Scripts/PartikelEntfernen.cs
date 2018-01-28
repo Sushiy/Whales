93158a251e2e3ff9baf917bed9711a7adb5172c8
Sushiy <julivizi@yahoo.de>
2018-01-28 04:02:11 +0100

    watersplashaz

---
+++ Assets/Scripts/PartikelEntfernen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartikelEntfernen : MonoBehaviour
+{
+    ParticleSystem partikel;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        partikel = GetComponent<ParticleSystem>();	
+	}
+	
+
+	// Zerstöre dich selbst, sobald dein Partikelsystem zuende ist
+	void Update ()
+    {
+        if (!partikel.IsAlive())
+            Destroy(transform.root.gameObject);
+	}
+}
--- Assets/Scripts/SurfaceBreachTrigger.cs
+++ Assets/Scripts/SurfaceBreachTrigger.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class SurfaceBreachTrigger : MonoBehaviour
 {
+    public GameObject prefab;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.layer == 8)
+        if (other.gameObject.layer == 8)
+        {
             ClipPlayer.s_clipPlayer.PlaySurface();
+            GameObject g = Instantiate(prefab, other.transform.position + new Vector3(0, 2.0f, 0), Quaternion.Euler(-90,0,0));
+        }
+
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.layer == 8)
+        {
             ClipPlayer.s_clipPlayer.PlaySurface();
+            GameObject g = Instantiate(prefab, other.transform.position + new Vector3(0, 1.0f,0), Quaternion.Euler(-90, 0, 0));
+        }
     }
 }
