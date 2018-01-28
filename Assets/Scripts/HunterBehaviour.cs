41d1d19635482b9b018bb9cebb65cfd082f56103
Kanaduchi-Kun <syrx@gmx.de>
2018-01-28 03:08:01 +0100

    Hunter Behaviour added
    hunter behaviour added

---
+++ Assets/Scripts/HunterBehaviour.cs
@@ -0,0 +1,233 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterBehaviour : MonoBehaviour {
+
+
+    public Transform waterHole1;
+    public Transform waterHole2;
+    public GameObject focusedTarget;
+
+    public bool isTargetingLeftHole;
+
+    public int currentState; // waiting left, waiting right, attentively, aiming, shooting, reloading
+
+    public bool isAttacking;
+    public bool isLeft;
+    public bool isRight;
+    public bool isAlarmed;
+    public bool isWalking;
+
+    public bool getsSignal;
+
+
+    float velocity;
+    public float threshhold;
+
+    
+
+    void Awake()
+    {
+        //waterEntrance = new Vector2(waterHole1.transform.position.x, waterHole1.transform.position.y); 
+
+        currentState = 0;
+        velocity = 0.05f;
+
+      
+
+        isAlarmed = false;
+        isWalking = false;
+        isLeft = true;
+        isRight = false;
+        isAttacking = false;
+        getsSignal = false;
+
+        threshhold = 5.0f;
+       
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        if (GetComponent<SpriteRenderer>().flipX == true)
+        {
+            isTargetingLeftHole = true;
+        }
+        else
+        {
+            isTargetingLeftHole = false;
+        }
+            
+        // Debug.Log(getDistance() + "");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //Debug.Log(new Vector2(focusedTarget.transform.position.x - waterHole1.position.x, focusedTarget.transform.position.y - waterHole1.position.y).magnitude + "Distance to Hole1");
+        //Debug.Log(new Vector2(focusedTarget.transform.position.x - waterHole2.position.x, focusedTarget.transform.position.y - waterHole2.position.y).magnitude + "Distance to Hole2");
+
+        switch (currentState)
+        {
+            //alarmed
+            case 0:
+                // signal comes in which hole is nearer?
+                isWalking = false;
+                playAnimation("focused");
+
+                if (getsSignal)
+                {
+
+                    if (new Vector2(focusedTarget.transform.position.x + waterHole1.position.x, focusedTarget.transform.position.y + waterHole1.position.y).magnitude <= new Vector2(focusedTarget.transform.position.x + waterHole2.position.x, focusedTarget.transform.position.y + waterHole2.position.y).magnitude)
+                    {
+                        //linkes loch
+                        if (isLeft)
+                        {
+                            isTargetingLeftHole = true;
+
+                            Debug.Log(new Vector2(focusedTarget.transform.position.x + transform.position.x, focusedTarget.transform.position.y + transform.position.y).magnitude);
+                            // already left hole
+                            if (new Vector2(focusedTarget.transform.position.x + transform.position.x, focusedTarget.transform.position.y + transform.position.y).magnitude <= threshhold)
+                            {
+                                currentState = 3;
+                                getsSignal = false;
+                            }
+                        }
+                        else
+                        {
+                            // he is right and needs to move left
+
+                            //moveLeft
+                            currentState = 1;
+                        }
+                    }
+                    else
+                    {
+                        //linkes loch
+                        if (isLeft)
+                        {
+                            Debug.Log(new Vector2(focusedTarget.transform.position.x + transform.position.x, focusedTarget.transform.position.y + transform.position.y).magnitude);
+                            // he is left and needs to move right
+                            if (new Vector2(focusedTarget.transform.position.x + transform.position.x, focusedTarget.transform.position.y + transform.position.y).magnitude <= threshhold)
+                            {
+                                currentState = 3;
+                                getsSignal = false;
+                            }
+                        }
+                        else
+                        {
+                           
+                            // already right hole
+                            //moveLeft
+                            currentState = 2;
+                        }
+
+                        //rechtes loch
+                    }
+                    // elseif target ist sehr nahe, dann 
+                    // currentState = 3;
+
+                }
+
+
+
+
+                break;
+            // walk to the left
+            case 1:
+                isWalking = true;
+
+                if (!isTargetingLeftHole)
+                {
+                    isTargetingLeftHole = true;
+                    GetComponent<SpriteRenderer>().flipX = true;
+                }
+
+                if (transform.position.x > waterHole1.position.x)
+                {
+                    playAnimation("walking");
+                    transform.Translate(-velocity, 0.0f, 0.0f);
+                }
+                else
+                {
+                    playAnimation("focused");
+                    currentState = 0;
+                }
+                // spielgele hunter
+                // starte lauf animation
+                
+                //transform.position.x = waterHole1.position.x;
+
+                //wenn da stoppe die laufanimation
+
+                break;
+            
+            //walk to the right 
+            case 2:
+                isWalking = true;
+                
+                if (isTargetingLeftHole)
+                {
+                    isTargetingLeftHole = false;
+                    GetComponent<SpriteRenderer>().flipX = false;
+                }
+
+                playAnimation("focused");
+                if (transform.position.x < waterHole2.position.x)
+                {
+                    playAnimation("walking");
+                    transform.Translate(velocity, 0.0f, 0.0f);
+                }
+                else
+                {
+                    playAnimation("focused");
+                    currentState = 0;
+                }
+                //figur spiegelen?
+                
+                // laufanimation an
+
+                break;
+
+            //attacking 
+            case 3:
+                playAnimation("Attack");
+                // Abspielen der Angriffsanimation Animation
+                currentState = 0;
+                break;
+
+            default: break;
+
+        }
+
+
+	}
+
+   // float getDistance()
+   // {
+   //     return new Vector2(waterEntrance.x + transform.position.x, waterEntrance.y + transform.position.y).magnitude;
+   // }
+
+    void playAnimation(string anima)
+    {
+
+        GetComponent<Animator>().SetTrigger(anima);
+
+
+    }
+
+    void enableCollider()
+    {
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+
+    void disableCollider()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/ISender.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISender {
+
+
+
+    // Use this for initialization
+    void send(IReceiver receiver, int message);
+}
